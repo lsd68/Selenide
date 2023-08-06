87663e8b7343ee445f3e6551f9dff24a46aa2f00
dmitry <whtperson@gmail.com>
2023-08-06 11:40:10 +0300

    commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+
+    @Test
+    public void SuccessCompleted() {
+        open("http://0.0.0.0:9999");
+        $("[data-test-id='city']input").setValue("Тамбов");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date']input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date']input").sendKeys(currentDate);
+        $("[data-test-id='name']input").setValue("Дмитрий Сергеевич");
+        $("[data-test-id='phone']input").setValue("+71234567890");
+        $("[data-test-id='agreement']").click();
+        $("button,button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+    }
+}
